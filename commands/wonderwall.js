9dd8e8f7119ad65103015fc7fdccb837d5e098ad
Sebirlinem <seb@sebirlinem.com>
2020-06-06 23:20:02 +1000

    anyway here's wonderwall
    added the wonderwall command

---
+++ commands/wonderwall.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'wonderwall',
+	description: '#1 alternate use for a dildo.',
+	execute(message, args) {
+		message.channel.send('https://www.youtube.com/watch?v=W_FRFLxWN3k');
+	},
+};
